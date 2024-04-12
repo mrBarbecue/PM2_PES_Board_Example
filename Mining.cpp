0ab787bdaa0a4e439cf4dcef2c78a466191bcacb
mrBarbecue <56227760+mrBarbecue@users.noreply.github.com>
2024-04-12 08:51:23 +0200

    solutionW2

--- Mining.cpp
+++ Mining.cpp
@@ -24,7 +24,7 @@ class Mining{
         const float voltageMax = 12.0f;
         const float maxVelocity = 1.0f;
         const float standardVelocity = 8.13f / voltageMax; // soll 350rpm, 5rps
-        const float threadedRodlengtMm = ???f;
+        const float threadedRodlengtMm = 80.0f;
         const float wheelUpperPos = threadedRodlengtMm / (1.25f * gearRatioMotorLiftWheel * 20); //Encoder Position Rad ganz oben, (20 weil 20 Encoderimpulse/umdrehung)
 
     
